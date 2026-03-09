b0557770cd8a37b0a37a858178ee69745a593b3b
felipe chaux <felipechauxx@misena.edu.co>
2026-03-09 18:07:51 -0500

    Initial commit with clean architecture setup

---
+++ android/app/src/main/kotlin/com/example/journalist_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.journalist_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/daily_news/domain/usecases/remove_article.dart
@@ -0,0 +1,14 @@
+import 'package:journalist_app/core/usecase/usecase.dart';
+import 'package:journalist_app/features/daily_news/domain/entities/article.dart';
+import 'package:journalist_app/features/daily_news/domain/repository/article_repository.dart';
+
+class RemoveArticleUseCase implements UseCase<void, ArticleEntity> {
+  final ArticleRepository _articleRepository;
+
+  RemoveArticleUseCase(this._articleRepository);
+
+  @override
+  Future<void> call({ArticleEntity? params}) {
+    return _articleRepository.removeArticle(params!);
+  }
+}
